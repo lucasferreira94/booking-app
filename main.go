15d5f094a5fb37225a86a357d39945f900d3ff86
lucasferreira94 <lucas_sferreira94@hotmail.com>
2022-12-26 08:58:25 -0300

    arrays pointers e booking tickets

--- main.go
+++ main.go
@@ -7,17 +7,36 @@ func main() {
 	const conferenceTicket = 50         // Valor constante, que não pode ser alterado
 	var remaningTickets uint = 50       // Valor uint pois essa variável não poderá conter valor negativo
 
-	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferceName is %T\n", conferenceTicket, remaningTickets, confereceName)
-
 	fmt.Printf("Welcome to %v booking application\n", confereceName) // Output formatado. {%v} significa printar no valor padrão
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTicket, remaningTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
-	var userTickets int
-	// ask user for their name
+	var bookings = [50]string{} // Array de tamanho máximo 50 elementos com uma lista vazia para atualização dinâmica de acordo com a execução do programa
 
-	userName = "Tom"
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+	//userName := ""
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+
+	// ask user for their name
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	// ask user for their last name
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	// ask user for their email
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	// ask user for their tickets
+	fmt.Println("Enter number of tickets")
+	fmt.Scan(&userTickets)
+
+	remaningTickets = remaningTickets - userTickets
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v \n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remainend for %v\n", remaningTickets, confereceName)
 }
