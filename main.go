41615d1370cf213facf50ca1eb8df126a7e23d74
lucasferreira94 <lucas_sferreira94@hotmail.com>
2022-12-28 08:38:27 -0300

    add loop infinito

--- main.go
+++ main.go
@@ -11,32 +11,40 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTicket, remaningTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var bookings = [50]string{} // Array de tamanho máximo 50 elementos com uma lista vazia para atualização dinâmica de acordo com a execução do programa
+	for {
+		// declaração array
+		// var bookings [50]string
 
-	//userName := ""
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
+		// declaração slice
+		//var bookings []string
+		bookings := []string{}
 
-	// ask user for their name
-	fmt.Println("Enter your first name: ")
-	fmt.Scan(&firstName)
+		//userName := ""
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	// ask user for their last name
-	fmt.Println("Enter your last name: ")
-	fmt.Scan(&lastName)
+		// ask user for their name
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
 
-	// ask user for their email
-	fmt.Println("Enter your email address: ")
-	fmt.Scan(&email)
+		// ask user for their last name
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
 
-	// ask user for their tickets
-	fmt.Println("Enter number of tickets")
-	fmt.Scan(&userTickets)
+		// ask user for their email
+		fmt.Println("Enter your email address: ")
+		fmt.Scan(&email)
 
-	remaningTickets = remaningTickets - userTickets
+		// ask user for their tickets
+		fmt.Println("Enter number of tickets")
+		fmt.Scan(&userTickets)
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v \n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets remainend for %v\n", remaningTickets, confereceName)
+		remaningTickets = remaningTickets - userTickets
+		bookings = append(bookings, firstName+" "+lastName)
+
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v \n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remainend for %v\n", remaningTickets, confereceName)
+	}
 }
